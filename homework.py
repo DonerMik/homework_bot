a775c432cb92f743d63d8c69d0d70cda8b323356
Dzianis Mikutaitsis <denismik92@gmail.com>
2021-12-05 15:03:05 +0300

    v 3

--- homework.py
+++ homework.py
@@ -148,7 +148,7 @@ def main():
         sys.exit()
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time.time()) - 1200
     all_errors = []
     while True:
         try:
